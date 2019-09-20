1b4d84138e37051a0e7b53d829af3e090a0f8ec6
liqinglongs <924229679@qq.com>
2019-09-20 20:16:48 +0800

    evenloop

--- even loop/任务定义.js
+++ even loop/任务定义.js
@@ -23,10 +23,10 @@ console.log('2');//2
 
 /**
  * 1.这段代码作为宏任务，进入主线程
- * 2.先遇到setTimeOut 将其注册回调后，分发到宏任务Event Queue。
+ * 2.先遇到setTimeOut 将其注册回调后，分发到宏队列Event Queue。
  * 3.遇到 Promise new Promise立即执行，then函数分发到微任务 Event Queue.
  * 4.遇到 console.log()立即执行。
- * 5.整体代码script作为第一个宏任务结束，检测微任务，发现.then在问任务Even Queue内，执行。
+ * 5.整体代码script作为第一个宏任务结束，检测微任务，发现.then在微任务Even Queue内，执行。
  * 6.第一轮循环结束，开始第二轮，检测Event Queue，发现宏任务 Even Queue中 setTimeout回调函数，立即执行。
  * 7.结束
  */
